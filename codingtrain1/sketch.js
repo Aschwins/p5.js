49b7fefb69e2457e8cbb7a675f35942b9fa9cf39
Aschwin <aschwin.schilperoort@gmail.com>
2018-09-02 12:30:42 +0200

    making the first sketch of a dude

--- codingtrain1/sketch.js
+++ codingtrain1/sketch.js
@@ -1,7 +1,27 @@
 function setup() {
-	createCanvas(640, 360);
+  createCanvas(720, 720);
+  background(250);
 }
 
 function draw() {
-  rect(100,200,75,150);
+  // body
+  fill(255, 0, 0);
+  rect(340, 250, 40, 150);
+
+  // head
+  fill(0, 255, 0);
+  ellipse(360, 250 , 150, 150);
+
+  // eyes
+  fill(0, 0, 255);
+  ellipse(340, 220, 30, 30);
+  ellipse(380, 220, 30, 30);
+
+  // legs
+  line(340, 400, 320, 450);
+  line(380, 400, 400, 450);
+
+  // arms
+  line(340, 325, 300, 360);
+  line(380, 325, 420, 360);
 }
