375bb18025b931936e048ba9c107518f3646f5a3
Aschwin <aschwin.schilperoort@gmail.com>
2018-11-05 09:33:26 +0100

    done variables 21

--- codingtrain2/variables21.js
+++ codingtrain2/variables21.js
@@ -1,10 +1,26 @@
+// Setup function, happens just once!
 function setup(){
   createCanvas(600, 400);
-  background(100);
+  background(250, 250, 100);
 }
 
+// Draw runs over and over again, loops
 function draw() {
-  fill(255, 0, 0 );
-  ellipse(mouseX, 100, 100, 100);
-  rect(20,20,20);
+  // no outline
+  noStroke();
+  // fill with RGB and alpha
+  fill(250, 200, 200, 50);
+
+  // ellipse We use one variable here: mouseX
+  ellipse(mouseX, mouseY, 25, 25);
+
+  fill(200, 250, 200);
+  rect(400, 100, 50, 50);
+}
+
+// Is an event. Waits patiently untill event happens!
+// When mouse gets pressed background get drawn again!
+function mousePressed() {
+  background(250, 250, 100);
+
 }
