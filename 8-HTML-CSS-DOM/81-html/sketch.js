4144b36af93c6af04b4ec52835224f484003bd87
Aschwin <aschwin.schilperoort@gmail.com>
2018-11-15 09:32:11 +0100

    HTML-CSS-DOM init

--- 8-HTML-CSS-DOM/81-html/sketch.js
+++ 8-HTML-CSS-DOM/81-html/sketch.js
@@ -1,7 +1,28 @@
+let counter = 0;
+
 function setup() {
-	createCanvas(windowWidth, windowHeight);
+	createCanvas(600, 400);
+	r = createElement('h1', "Generate Random Numbers by pressing the Mouse!")
 }
 
 function draw() {
-
+	background(100);
+	rect(counter, 50, 200, 20);
 }
+
+function mousePressed() {
+	counter = counter + 1;
+	createP("Random number: " + random(0,10) );
+	r.html("Oh my god, there they are!!!")
+}
+
+// createCanvas()
+// createP()
+// createDiv()
+// createButton()
+// createImage(width, height)
+
+// something.position([x], [y])
+
+// something.html([html], [append])
+
