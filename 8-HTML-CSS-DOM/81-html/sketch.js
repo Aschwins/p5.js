14f1346ce92adbae8e8ad4570a7afe1f001ab0ae
Aschwin <aschwin.schilperoort@gmail.com>
2018-11-19 13:34:06 +0100

    adding a way to add a button

--- 8-HTML-CSS-DOM/81-html/sketch.js
+++ 8-HTML-CSS-DOM/81-html/sketch.js
@@ -1,21 +1,31 @@
+// How to create buttons and other interactive HTML elements with Javascript.
+
 let counter = 0;
+var bgcolor;
+var button;
 
 function setup() {
 	createCanvas(600, 400);
 	r = createElement('h1', "Generate Random Numbers by pressing the Mouse!")
+	button = createButton("Press here");
+	bgcolor = 100;
 }
 
 function draw() {
-	background(100);
+	background(bgcolor);
 	rect(counter, 50, 200, 20);
+	button.mousePressed(changebgColor);
 }
 
 function mousePressed() {
 	counter = counter + 1;
 	createP("Random number: " + random(0,10) );
-	r.html("Oh my god, there they are!!!")
+	r.html("Oh my god, there they are!!!");
 }
 
+function changebgColor() {
+	bgcolor = random(0,255);
+}
 // createCanvas()
 // createP()
 // createDiv()
@@ -26,3 +36,6 @@ function mousePressed() {
 
 // something.html([html], [append])
 
+// createButton("go go go")
+// use a callback. button.mousePressed()
+// 
