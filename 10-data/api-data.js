a94a27a297ee7e6f42638e37b4708315281a7c8d
Aschwin <aschwin.schilperoort@gmail.com>
2018-12-04 09:30:31 +0100

    adding API calls

---
+++ 10-data/api-data.js
@@ -0,0 +1,15 @@
+var url = 'http://api.openweathermap.org/data/2.5/weather?q=London&APPID=570b78367d0ddeb10b8f17fa92dbc7d5'
+
+function setup() {
+  loadJSON(url, gotData)
+
+}
+
+function gotData(data) {
+  console.log(data);
+}
+
+function draw() {
+
+
+}
--- 10-data/url-data.js
+++ 10-data/url-data.js
@@ -1,5 +1,7 @@
+var url = 'api.openweathermap.org/data/2.5/weather?q=London&APPID=570b78367d0ddeb10b8f17fa92dbc7d5'
+
 function setup() {
-  loadJSON("http://api.open-notify.org/astros.json", gotData)
+  loadJSON(url, gotData)
 
 }
 
