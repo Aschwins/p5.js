1252e2faf11f63a38c8db6992a0f389132e3f429
Aschwin <aschwin.schilperoort@gmail.com>
2018-12-04 10:07:15 +0100

    adding input API calls

--- 10-data/api-data.js
+++ 10-data/api-data.js
@@ -1,12 +1,19 @@
-var url = 'http://api.openweathermap.org/data/2.5/weather?q=London&APPID=570b78367d0ddeb10b8f17fa92dbc7d5'
+var base_url = 'http://api.openweathermap.org/data/2.5/weather?';
+var api_key = 'APPID=570b78367d0ddeb10b8f17fa92dbc7d5';
+
 
 function setup() {
-  loadJSON(url, gotData)
-
+  input = createInput();
+  input.changed(loadData);
 }
 
 function gotData(data) {
   console.log(data);
+}
+
+function loadData() {
+  var url = base_url + '&' + api_key + '&q=' + input.value();
+  loadJSON(url, gotData);
 }
 
 function draw() {
