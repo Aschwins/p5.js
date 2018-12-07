85d911541a8617f97718dafb511bb30a56d2eea1
Aschwin <aschwin.schilperoort@gmail.com>
2018-12-07 11:37:04 +0100

    adding a map to show where the iss is

--- 10-data/iss-api.js
+++ 10-data/iss-api.js
@@ -1,22 +1,69 @@
 var url = 'http://api.open-notify.org/iss-now.json';
 var api_key = 'APPID=570b78367d0ddeb10b8f17fa92dbc7d5';
 
+let myMap;
+let canvas;
 
-function setup() {
-loadData();
+const mappa = new Mappa('Leaflet');
+var iss;
+
+
+// All our map options
+const options = {
+  lat: 0,
+  lng: 0,
+  zoom: 1,
+  style: "http://{s}.tile.osm.org/{z}/{x}/{y}.png"
 }
 
+function setup() {
+  iss = new spaceStation(0, 0);
+
+  //loadData();
+  myMap = mappa.tileMap(options);
+  canvas = createCanvas(640, 640);
+  // Create a tile map center in New York with an initial zoom level of 10.
+
+  // Overlay the canvas to the new tile map created.
+  myMap.overlay(canvas);
+
+  myMap.onChange(drawPoint);
+
+}
+
+function draw() {
+  
+}
+
+function drawPoint() {
+  // clear Canvas when the new ISS location is known
+  clear();
+  const iss_location = myMap.latLngToPixel(iss.latitude, iss.longitude)
+  fill(0);
+  ellipse(iss_location.x, iss_location.y, 10, 10)
+}
+
+// Callback function after the iss-api call.
 function gotData(data) {
-  console.log(data);
+  console.log(data.iss_position.latitude, data.iss_position.longitude);
+  iss.latitude = data.iss_position.latitude
+  iss.longitude = data.iss_position.longitude
+  drawPoint();
 }
 
 function loadData() {
   loadJSON(url, gotData);
 }
 
-function draw() {
-
-
+class spaceStation {
+  constructor(lat, lon) {
+    this.latitude = lat;
+    this.longitude = lon;
+  }
 }
 
-setInterval(loadData, 1000);
+
+
+setInterval(loadData, 10000);
+
+//[Log] {message: "success", timestamp: 1544172735, iss_position: {latitude: "-44.0984", longitude: "-103.3333"}} (iss-api.js, line 35)
--- 10-data/url-data.js
+++ 10-data/url-data.js
@@ -1,7 +1,5 @@
-var url = 'api.openweathermap.org/data/2.5/weather?q=London&APPID=570b78367d0ddeb10b8f17fa92dbc7d5'
-
 function setup() {
-  loadJSON(url, gotData)
+  loadJSON("http://api.open-notify.org/astros.json", gotData)
 
 }
 
